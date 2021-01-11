5bb299766e3819926c9e87a07f9b69595aa75e8b
BornCreator <73579033+BornCreator@users.noreply.github.com>
2021-01-11 09:20:39 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,33 @@
-var leftWristX,leftWristY,rightWristX,rightWristY,song1,song2,leftWristScore,rightWristScore,songStatus;
+//Variables
+var leftWristX='';
+var leftWristY='';
+var rightWristX='';
+var rightWristY='';
+var song1='';
+var song2='';
+var leftWristScore='';
+var rightWristScore='';
+var song1PlayStatus='';
+var song2PlayStatus='';
+
+
+
+
+
+
+//Preload
 function preload(){
-    song1=loadSound('dj-nate-TOE 3.mp3');
-    song2=loadSound('Press Start(Dex Arson Remix).mp3');
+    song1=loadSound('Press Start(Dex Arson Remix).mp3');
+    song2=loadSound('dj-nate-TOE 3.mp3');
 }
+
+
+
+
+
+
+
+//Setup
 function setup(){
     canvas=createCanvas(500,450);
     canvas.position(425,300);
@@ -11,34 +36,42 @@ function setup(){
     poseNet=ml5.poseNet(video,modelLoaded);
     poseNet.on('pose',gotPoses);
 }
+
+
+
+
+
+
+
+
+
+//Draw function
 function draw(){
     image(video,0,0,500,450);
     fill('red');
     stroke('black');
     if(leftWristScore>0.05){
         square(leftWristX-200,leftWristY-200,10,10);
-        song2.stop();
         if(song1.isPlaying()==true){
-            console.log('SONG1 IS PLAYING');
             song1PlayStatus=true;
         }
         if(song1.isPlaying()==false){
-            console.log("SONG1 ISN'T PLAYING");
             song1PlayStatus=false;
             document.getElementById('song_name').innerHTML='Song: MDK - Press Start (Dex Arson Remix)';
+            song2.stop();
+            song1.play();
         }
     }
     if(rightWristScore>0.05){
         square(rightWristX-200,rightWristY-200,10,10);
-        song1.stop();
         if(song2.isPlaying()==true){
-            console.log('SONG2 IS PLAYING');
             song2PlayStatus=true;
         }
         if(song2.isPlaying()==false){
-            console.log("SONG2 ISN'T PLAYING");
             song2PlayStatus=false;
             document.getElementById('song_name').innerHTML='Song: F-777 Theory Of Everything 3';
+            song1.stop();
+            song2.play();
         }
     }
     if(song1.isPlaying()==true){
@@ -46,9 +79,19 @@ function draw(){
         song1PlayStatus=true;
     }
 }
+
+
+
+
+
+//ModelLoaded
 function modelLoaded(){
     console.log("MODEL HAS BEEN LOADED!!!");
 }
+
+
+
+//gotPoses
 function gotPoses(results){
     if(results.length>0){
         console.log(results);
